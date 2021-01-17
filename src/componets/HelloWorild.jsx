f538e37084006dfe496d8ada934a60549e2b5034
Camilo Pardo <camilopardochoachi@gmail.com>
2021-01-16 21:47:22 -0500

    Configuración inicial de desarrollo para empesar proyecto

---
+++ src/componets/HelloWorild.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../assets/styles/app.scss'
+const HelloWorld= () => (
+    <h1>Hola Alex</h1>
+    
+);
+
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './componets/HelloWorild'
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+
