ca03365f144166a02971dc2fc0c931f867ea7e7a
Camilo Pardo <contacto@parddo.com>
2021-03-17 17:50:09 -0500

    Eliminacion del area de PayU

--- src/componets/sections/Collaboration.jsx
+++ src/componets/sections/Collaboration.jsx
@@ -20,19 +20,6 @@ class Colaboration extends React.Component {
                         <h4>¡Muchas gracias!</h4>
                     </div>
                 </div>
-                <div>
-                    <div className="card text-center card-colaboration">
-                        <div className="card-body">
-                            
-                            <h3 className="card-title">Puedes ayudarme de forma segura con Pay<b>U</b></h3>
-                            <div className="btn-colaboration">
-                                <a href="https://biz.payulatam.com/L0e0d4d238006E4" target="_blank"> <button type="button" className="btn btn-success"><samp></samp> $20.000 Pay<b>U</b></button></a>
-                                <a href="https://biz.payulatam.com/L0e0d4dBD9AFAFE" target="_blank"> <button type="button" className="btn btn-success "><samp></samp> $50.000 Pay<b>U</b></button></a>
-                                <a href="https://biz.payulatam.com/L0e0d4d405A4742" target="_blank"> <button type="button" className="btn btn-success btn-lg btn-block"><samp></samp> $100.000 Pay<b>U</b></button></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </section>
         )
     };
