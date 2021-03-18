33e736860ebf7f663d8ef6293b205ca1857fa6a1
Camilo Pardo <contacto@parddo.com>
2021-03-17 20:29:10 -0500

    Modificacion del enlace en la imagen del colaboration

--- src/componets/sections/Collaboration.jsx
+++ src/componets/sections/Collaboration.jsx
@@ -12,7 +12,7 @@ class Colaboration extends React.Component {
                 <SectionOne icoClass="fas fa-business-time" tlite="Busco empleo" description="Cuento con experiencia como auxiliar administrativo y técnico de soporte en servicios de alojamiento web, actualmente vivo en la ciudad de Medellín, pero cuento con disponibilidad para mudarme a otras ciudades como Bogotá o Cali en caso de que sea necesario." />
                 <div className="sectionOne history">
                     <div className="hisotory-photo">
-                        <img className="hisotory-photo__img" src="https://scontent.feoh1-1.fna.fbcdn.net/v/t1.0-9/143868746_109425014468986_3537681929869559314_o.jpg?_nc_cat=106&ccb=1-3&_nc_sid=8bfeb9&_nc_eui2=AeFOKpReVgBXUI7ugHnwGN8lpUCJIt7WHy6lQIki3tYfLv11NCNIOA0sqxAylpeVIZ2XHRJzvRYrTiaQz0Ads3By&_nc_ohc=w1qOV1yF7_kAX8xkSBz&_nc_ht=scontent.feoh1-1.fna&oh=db1f0146af9dcf3b1e1a509cb9db027d&oe=6075C765" alt=""></img>
+                        <img className="hisotory-photo__img" src="https://parddo.com/img/photo.jpg" alt=""></img>
                     </div>
                     <div className="hisotory-description">
                         <p className="hisotory-description__content">Decidí buscar empleo de esta manera, ya que en estos momentos se me ha dificultado para todo lo que ha sido mi alimentación, el arriendo, transporte, entre otros. También tomé esta decisión, porque creo que no hay justificación alguna para renunciar a mis estudios y dejar de perseguir mis sueños y aspiraciones.</p>
